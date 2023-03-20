98e70f51f9db09efdd43b9580e5e403531e7a10f
rajatbhardwaj1 <rb9878203799@gmail.com>
2023-03-20 11:14:49 +0530

    data

---
+++ fill_table.sql
@@ -0,0 +1,8 @@
+COPY Person from '/home/project/power-saver-mode.h/data/Person.csv' DELIMITER '|' CSV HEADER;
+COPY Groups from '/home/project/power-saver-mode.h/data/Groups.csv' DELIMITER '|' CSV HEADER;
+COPY Person_Belongsto_Group from '/home/project/power-saver-mode.h/data/Person_Belongsto_Group.csv' DELIMITER '|' CSV HEADER;
+COPY Posts from '/home/project/power-saver-mode.h/data/Posts.csv' DELIMITER '|' CSV HEADER;
+COPY Comments from '/home/project/power-saver-mode.h/data/Comments.csv' DELIMITER '|' CSV HEADER;
+COPY Person_Likes_Post from '/home/project/power-saver-mode.h/data/Person_Likes_Post.csv' DELIMITER '|' CSV HEADER;
+COPY Person_Likes_Comment from '/home/project/power-saver-mode.h/data/Person_Likes_Comment.csv' DELIMITER '|' CSV HEADER;
+COPY Friends from '/home/project/power-saver-mode.h/data/Friends.csv' DELIMITER '|' CSV HEADER;
